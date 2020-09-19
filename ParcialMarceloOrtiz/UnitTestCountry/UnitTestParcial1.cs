81bcf6feac319495c5c37413ea506afc48fe92a9
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2020-09-19 12:14:55 -0400

    Country con UniteTest

---
+++ ParcialMarceloOrtiz/UnitTestCountry/UnitTestParcial1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialMarceloOrtiz.Controllers;
+
+namespace UnitTestCountry
+{
+    [TestClass]
+    public class UnitTestParcial1
+    {
+        [TestMethod]
+        public void TestGetCountry()
+        {
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+            //Act
+            var pruebacountry = countriesController.GetCountries();
+            //Assert
+            Assert.IsNotNull(GetCountries);
+        }
+    }
+}
